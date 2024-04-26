3f893a8dbe69560bea631a9c364dbb81b028404e
Joluga004 <166079213+Joluga004@users.noreply.github.com>
2024-04-26 00:34:54 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Mode of Transportation
+// Author: Jorge L. Garcia
+// Date: April 25th 2024
 
 // Constants
 
@@ -19,3 +19,19 @@ function main() {
 
 // let's get this party started
 main();
+
+//Define Variables
+make = "Loop bus";
+model = "25 foot Automatic";
+color = "white";
+year =  1992;
+
+// calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
