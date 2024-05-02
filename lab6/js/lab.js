1089fe151ebc4fca594ba9e9486d3881883f05dd
Joluga004 <166079213+Joluga004@users.noreply.github.com>
2024-05-02 03:02:10 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,8 +1,10 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Arrays and Objects
+// Author: Jorge L. Garcia
+// Date: 05/01/24
 
 // Constants
+
+
 
 // Functions
 
@@ -19,3 +21,24 @@ function main() {
 
 // let's get this party started
 main();
+
+// Define Variables
+myTransport = ['ebike', 'loop bus', 'city bus', 'walking']
+
+//create an object for my main ride
+let myMainRide = {
+  make: "loop bus",
+  model: "25 foot Automatic",
+  color: "white",
+  year: 1992,
+  age: function() {
+      return currentYear - this.year;
+  }
+}
+
+//output
+document.writeln("kinds of transportation I use: ", myTransport,"</br>");
+//this little trick allos us to write an object to the document
+document.writeln("My Main Ride: <pre>"
+  JSON.stringify(myMainRide, null, '/t'), "</pre>");
+  
