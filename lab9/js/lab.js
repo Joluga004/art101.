70ac9a5084f8e1de40558a10ceb43589d70f3023
Joluga004 <166079213+Joluga004@users.noreply.github.com>
2024-05-16 23:04:03 -0700

    ninenine

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,24 +2,41 @@
 // Author: Jorge L. Garcia
 // Date:05/06/2024
 
+// Using jQuery, create buttons to add to the challenge, problems, and results sections of your document:
+// Find the section you want to add the button to
+// Create the button
+// Give it a text label
+// Add it to the section
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
 
+// Add to your previous JavaScript file:
+// Using jQuery, do the following for each button:
+// Add a click event to each button
+// Find the challenge, problems, or results section the button is in.
+// Toggle a class special within the <div> of the section
+// Go into your css and style each of the classes
+// Test each button
+$(".highlight-button").click(function(){
+  // I can use "this" to refer to the button that got clicked
+  // here's a different maybe better way to do it
+  // (we are traversing the DOM to the parent)
+  $(this).parent().toggleClass("special");
+})
 
-function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
-}
+// Now let's add a handler for the party toggle button
+$("#party-toggle button").click(function(){
+  // if the body element has a class of party...
+  // then change button text to "Business Time"
+  if ($("body").hasClass("party")) {
+    $("body").removeClass("party");
+    $("#party-toggle button").html("Party");
+  }
+  else {
+    $("body").addClass("party");
+    $("#party-toggle button").html("Business");
+  }
+})
 
-// click listener for button
-$("#make-convo").click(function(){
-	console.log("click");
-	// get new fake dialogue
-	const newText = generateRandomText();
-  // append a new div to our output div
-	$("#output").append('<div class="text"><p>' + newText + '</p></div>');
-});
+$("#make-hide").click(function(){
+  $("#instructions").toggleClass("hidden");
+})  
